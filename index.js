b75f5137c6ee3e62fabde8c397ee14deeb0f8988
Azat Mardan <hi@azat.co>
2017-11-26 16:14:24 -0600

    why they always have to change everything!!! updating to \u001b to avoid octal literal in strict mode ES6 error

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ var _console = global.console
 
 // Overwrite the object with color logs
 var colors = [{name: 'log',
-  color: '\033[0m'
+  color: '\u001b[0m'
 },{name: 'info',
-  color: '\033[36m'
+  color: '\u001b[36m'
 },{name: 'warn',
-  color: '\033[33m'
+  color: '\u001b[33m'
 },{name: 'error',
-  color: '\033[31m'
+  color: '\u001b[31m'
 }]
 
 colors.forEach((mode) => {
   return global[mode.name] = function() {
     var args = arguments
     // _console.log(args, mode)
-    args[0] = mode.color +args[0] + '\x1b[0m'
+    args[0] = mode.color +args[0] + '\u001b[0m'
     // _console.log(args, mode)
     return _console[mode.name].apply(null, args)
   }
